e1a9a6dbd07be11bc76e809fe4b69579ef3acc77
Sergey Georgiev <sergeygeorgiev1709@gmail.com>
2023-04-16 15:04:30 +0300

    refactor: delete unused imports

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,11 +1,5 @@
 package tictactoe;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
 
     public static void main(String[] args) {
